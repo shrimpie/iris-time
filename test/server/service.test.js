5135deae7ed175b0b68a7ac5217318a3a61f1d4c
杨振宇 <andy@yang.local>
2017-04-01 13:05:04 -0400

    Changed single quotes to double

--- test/server/service.test.js
+++ test/server/service.test.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-require('should');
-const request = require('supertest');
+require("should");
+const request = require("supertest");
 const config = require("../../config");
-const service = require('../../server/service')(config);
+const service = require("../../server/service")(config);
 
-describe('The express service', () => {
-    describe('GET /foo', () => {
-        it('should return HTTP 404', (done) => {
+describe("The express service", () => {
+    describe("GET /foo", () => {
+        it("should return HTTP 404", (done) => {
             request(service)
-            .get('/foo')
+            .get("/foo")
             .expect(404, done);
         });
     });
 
-    describe('GET /service/:location', () => {
-        it('should return HTTP 200 and a reply with a valid result', (done) => {
+    describe("GET /service/:location", () => {
+        it("should return HTTP 200 and a reply with a valid result", (done) => {
             request(service)
-            .get('/service/vienna')
+            .get("/service/vienna")
             .expect(200)
             .end((err, res) => {
                 if(err) return done(err);
